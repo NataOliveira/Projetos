6836dfefb48c7119b1820f02ec9169465e86110c
NataOliveira <natanvsnatan@gmail.com>
2022-01-27 20:28:46 -0300

    calculadora furreca usando switch

---
+++ firtsprojects/Calculadora com switch em c/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,x,z;
+printf("******************Calculadora******************\n");
+printf(" 1             MULTIPLICACAO\n 2             DIVISAO\n 3             ADICAO\n 4             SUBTRACAO\n\n\n");
+scanf("%i", &x);
+
+switch(x){
+    case 1:
+    printf("\nMultiplicacao\n");
+    scanf("%i",&a);
+    scanf("%i",&b);
+    z = a*b;
+    printf("\nA multiplicacao de ""%i" " e " "%i" " ,igual a : " "%i",a,b,z);
+
+    break;
+
+    case 2:
+    printf("\divisao\n");
+    scanf("%i",&a);
+    scanf("%i",&b);
+    z = a/b;
+    printf("%i",z);
+    printf("\nA divisao de ""%i" " e " "%i" " ,igual a : " "%i",a,b,z);
+    break;
+
+    case 3:
+    printf("\nadicao\n");
+    scanf("%i",&a);
+    scanf("%i",&b);
+    z = a+b;
+    printf("%i",z);
+    printf("\nA adicao de ""%i" " e " "%i" " ,igual a : " "%i",a,b,z);
+    break;
+
+    case 4:
+    printf("\nsubtracao\n");
+    scanf("%i",&a);
+    scanf("%i",&b);
+    z = a-b;
+    printf("%i",z);
+    printf("\nA subtracao de ""%i" " e " "%i" " ,igual a : " "%i",a,b,z);
+    break;
+
+    default:
+    printf("ERROR");
+}
+   return 0;
+}
