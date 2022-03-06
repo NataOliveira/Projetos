2c115593f387e4f5b8f70a5cc0e6fbf6ae45e407
NataOliveira <natanvsnatan@gmail.com>
2022-03-06 17:28:26 -0300

    calculadora js

---
+++ Calculadora JS/script.js
@@ -0,0 +1,47 @@
+//numéricos
+var n1 = document.getElementById ('1')
+var n2 = document.getElementById ('2')
+var n3 = document.getElementById ('3')
+var n4 = document.getElementById ('4')
+var n5 = document.getElementById ('5')
+var n6 = document.getElementById ('6')
+var n7 = document.getElementById ('7')
+var n8 = document.getElementById ('8')
+var n9 = document.getElementById ('9')
+var n0 = document.getElementById ('0')
+var point = document.getElementById('.')
+
+n1 = 1
+n2 = 2
+n3 = 3
+n4 = 4
+n5 = 5
+n6 = 6
+n7 = 7
+n8 = 8
+n9 = 9
+n0 = 0
+
+//funcoes
+
+function c(){ document.getElementById('tela').value = ''}
+function porcento(){}
+function divisao(){}
+function fatorial(){}
+function sete(){document.getElementById('tela').value += '7'}
+function oito(){document.getElementById('tela').value += '8'}
+function nove(){document.getElementById('tela').value += '9'}
+function x(){}
+function quatro(){document.getElementById('tela').value += '4'}
+function cinco(){document.getElementById('tela').value += '5'}
+function seis(){document.getElementById('tela').value += '6'}
+function menos(){}
+function um(){document.getElementById('tela').value += '1'}
+function dois(){document.getElementById('tela').value += '2'}
+function tres(){document.getElementById('tela').value += '3'}
+function mais(){}
+function ponto(){document.getElementById('tela').value += '.'}
+function zero(){document.getElementById('tela').value += '0'}
+function p(){}
+function igual(){}
+
